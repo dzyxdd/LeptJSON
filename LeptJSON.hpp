fd6f540a27919b7573fb2081f9846df3fd570f7a
DingZY <331646081@qq.com>
2024-03-11 02:48:43 +0800

    finish tutorial03

--- LeptJSON.hpp
+++ LeptJSON.hpp
@@ -10,39 +10,85 @@
 #include <cctype>
 #include <variant>
 #include <string_view>
-
-#define JSON_VALUE std::visit(visitor, jsonValue.value)
+#include <stdexcept>
+#include <cmath>
+#include <stack>
 
 struct LeptJSON {
 
 	enum class ValueType { NULL_TYPE, FALSE_TYPE, TRUE_TYPE, NUMBER_TYPE, STRING_TYPE, ARRAY_TYPE, OBJECT_TYPE };
 
-	enum class Status { PARSE_OK = 0, PARSE_EXPECT_VALUE, PARSE_INVALID_VALUE, PARSE_ROOT_NOT_SINGULAR, PARSE_NUMBER_TOO_BIG };
+	enum class Status {
+		PARSE_OK, PARSE_EXPECT_VALUE, PARSE_INVALID_VALUE, PARSE_ROOT_NOT_SINGULAR, PARSE_NUMBER_TOO_BIG, PARSE_MISS_QUOTATION_MARK, PARSE_INVALID_STRING_ESCAPE, PARSE_INVALID_STRING_CHAR, PARSE_INVALID_UNICODE_HEX
+	};
 
 private:
-	struct {
-		std::variant<double>value;
+	using jsonValueType = std::variant<std::nullptr_t, double, std::string, bool>;
+	struct JsonValue {
+		jsonValueType value;
 		ValueType type;
 	}jsonValue;
 
-	inline static auto visitor = []<typename _Ty>(_Ty && arg)->_Ty { return arg; };
+	struct Visitor {
+		jsonValueType operator()(double arg) const {
+			return arg;
+		}
+
+		jsonValueType operator()(std::string arg) const {
+			return arg;
+		}
+
+		template<typename T>
+		jsonValueType operator()(T) const {
+			throw std::runtime_error("Wrong Variant Type");
+		}
+	};
 
 	std::string_view json;
 
 public:
-	explicit LeptJSON(const char* js, ValueType vt = ValueType::NULL_TYPE)
+	explicit LeptJSON(const char* js = "", ValueType vt = ValueType::NULL_TYPE)
 		:jsonValue({}, vt), json(js) {}
 
 	ValueType get_type()const {
 		return jsonValue.type;
 	}
 
-	double get_number()const {
-		assert(jsonValue.type == ValueType::NUMBER_TYPE);
-		return JSON_VALUE;
+	void set_value(const ValueType vt, const jsonValueType& v) {
+		jsonValue.type = vt;
+		jsonValue.value = v;
 	}
 
+	void set_nullptr() {
+		set_value(ValueType::NULL_TYPE, nullptr);
+	}
 
+	bool get_boolean() const {
+		assert(jsonValue.type == ValueType::TRUE_TYPE || jsonValue.type == ValueType::FALSE_TYPE);
+		return std::get<bool>(jsonValue.value);
+	}
+
+	void set_boolean(bool b) {
+		set_value(b ? ValueType::TRUE_TYPE : ValueType::FALSE_TYPE, b);
+	}
+
+	double get_number() const {
+		assert(jsonValue.type == ValueType::NUMBER_TYPE);
+		return std::get<double>(jsonValue.value);
+	}
+
+	void set_number(double number) {
+		set_value(ValueType::NUMBER_TYPE, number);
+	}
+
+	std::string_view get_string()const {
+		assert(jsonValue.type == ValueType::STRING_TYPE);
+		return std::get<std::string>(jsonValue.value);
+	}
+
+	void set_string(std::string str) {
+		set_value(ValueType::STRING_TYPE, str);
+	}
 
 	Status parse() {
 		jsonValue.type = ValueType::NULL_TYPE;
@@ -58,21 +104,17 @@ public:
 		return ret;
 	}
 
-	Status parse(const char* input) {
-		json = input;
-		return parse();
-	}
-
 private:
 	/* value = null / false / true / number */
 	Status parse_value() {
 		if (json.empty())return Status::PARSE_EXPECT_VALUE;
 		switch (json[0]) {
-		case 't':return parse_literal("true", ValueType::TRUE_TYPE);
-		case 'f':return parse_literal("false", ValueType::FALSE_TYPE);
-		case 'n':return parse_literal("null", ValueType::NULL_TYPE);
-		case '\0':return Status::PARSE_EXPECT_VALUE;
-		default:return parse_number();
+			case 't':return parse_literal("true", ValueType::TRUE_TYPE);
+			case 'f':return parse_literal("false", ValueType::FALSE_TYPE);
+			case 'n':return parse_literal("null", ValueType::NULL_TYPE);
+			case '\0':return Status::PARSE_EXPECT_VALUE;
+			case '"':return parse_string();
+			default:return parse_number();
 		}
 	}
 
@@ -85,7 +127,9 @@ private:
 
 	/* literal = "null" / "false" / "true" */
 	Status parse_literal(std::string_view literal, ValueType type) {
-		if (json.size() < literal.size())return Status::PARSE_INVALID_VALUE;
+		if (json.size() < literal.size()) {
+			return Status::PARSE_INVALID_VALUE;
+		}
 		for (size_t i = 0; i < literal.size(); ++i) {
 			if (json[i] != literal[i]) {
 				return Status::PARSE_INVALID_VALUE;
@@ -97,43 +141,87 @@ private:
 	}
 
 	/*
-	 * number = [ "-" ] int [ frac ] [ exp ]
-	 * int = "0" / digit1-9 *digit
-	 * frac = "." 1*digit
-	 * exp = ("e" / "E") ["-" / "+"] 1*digit
-	 */
+  * number = [ "-" ] int [ frac ] [ exp ]
+  * int = "0" / digit1-9 *digit
+  * frac = "." 1*digit
+  * exp = ("e" / "E") ["-" / "+"] 1*digit
+  */
 	Status parse_number() {
 		std::string_view judge = json;
-		if (judge.starts_with('-')) judge.remove_prefix(1);
-		if (judge.starts_with('0')) judge.remove_prefix(1);
+		if (judge.starts_with('-')) {
+			judge.remove_prefix(1);
+		}
+		if (judge.starts_with('0')) {
+			judge.remove_prefix(1);
+		}
 		else {
-			if (judge.empty() || !isdigit(judge[0])) return Status::PARSE_INVALID_VALUE;
+			if (judge.empty() || !isdigit(judge[0])) {
+				return Status::PARSE_INVALID_VALUE;
+			}
 			for (judge.remove_prefix(1); judge.size() && isdigit(judge[0]); judge.remove_prefix(1));
 		}
 		if (judge.starts_with('.')) {
 			judge.remove_prefix(1);
-			if (judge.empty() || !isdigit(judge[0])) return Status::PARSE_INVALID_VALUE;
+			if (judge.empty() || !isdigit(judge[0])) {
+				return Status::PARSE_INVALID_VALUE;
+			}
 			for (judge.remove_prefix(1); judge.size() && isdigit(judge[0]); judge.remove_prefix(1));
 		}
 		if (judge.starts_with('e') || judge.starts_with('E')) {
 			judge.remove_prefix(1);
-			if (judge.starts_with('+') || judge.starts_with('-')) judge.remove_prefix(1);
-			if (judge.empty() || !isdigit(judge[0])) return Status::PARSE_INVALID_VALUE;
+			if (judge.starts_with('+') || judge.starts_with('-')) {
+				judge.remove_prefix(1);
+			}
+			if (judge.empty() || !isdigit(judge[0])) {
+				return Status::PARSE_INVALID_VALUE;
+			}
 			for (judge.remove_prefix(1); judge.size() && isdigit(judge[0]); judge.remove_prefix(1));
 		}
 
 		errno = 0;
 		jsonValue.value = strtod(json.data(), nullptr);
-		if (errno == ERANGE && (JSON_VALUE == HUGE_VAL || JSON_VALUE == -HUGE_VAL)) {
+		if (errno == ERANGE && (std::get<double>(jsonValue.value) == HUGE_VAL || std::get<double>(jsonValue.value) == -HUGE_VAL)) {
 			return Status::PARSE_NUMBER_TOO_BIG;
 		}
 		json = judge;
 		jsonValue.type = ValueType::NUMBER_TYPE;
 		return Status::PARSE_OK;
 	}
+
+	Status parse_string() {
+		std::string s;
+		if (json.starts_with('\"'))
+			json.remove_prefix(1);
+		for (; json.size(); json.remove_prefix(1)) {
+			switch (json.front()) {
+				case '\"':
+					jsonValue = { s,ValueType::STRING_TYPE };
+					json.remove_prefix(1);
+					return Status::PARSE_OK;
+				case '\\':
+					json.remove_prefix(1);
+					if (json.empty())return Status::PARSE_INVALID_STRING_ESCAPE;
+					switch (json.front()) {
+						default:return Status::PARSE_INVALID_STRING_ESCAPE;
+						case'\"': s += '\"'; break;
+						case'\\':s += '\\'; break;
+						case'/':s += '/'; break;
+						case'b':s += '\b'; break;
+						case'f':s += '\f'; break;
+						case'n':s += '\n'; break;
+						case'r':s += '\r'; break;
+						case't':s += '\t'; break;
+					}
+					break;
+				default:
+					if (static_cast<unsigned char>(json.front()) < 0x20) {
+						return Status::PARSE_INVALID_STRING_CHAR;
+					}
+					s += json.front();
+			}
+		}
+		return Status::PARSE_MISS_QUOTATION_MARK;
+	}
 };
 
-
-
 #endif/* _LEPTJSON_H_ */
-
--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 
 #include "LeptJSON.hpp"
+#include <iostream>
 
 static int main_ret = 0;
 static int test_count = 0;
@@ -15,7 +16,6 @@ using ValueType = LeptJSON::ValueType;
 #define EXPECT_EQ_BASE(equality, expect, actual, format) \
     do {\
         test_count++;\
-		printf("%d\n: ",test_count);\
         if (equality)\
             test_pass++;\
         else {\
@@ -26,6 +26,9 @@ using ValueType = LeptJSON::ValueType;
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 #define EXPECT_EQ_DOUBLE(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%.17g")
+#define EXPECT_EQ_STRING(expect, actual) EXPECT_EQ_BASE(expect == actual, expect, actual.data(), "%s")
+#define EXPECT_TRUE(actual) EXPECT_EQ_BASE((actual) != 0, "true", "false", "%s")
+#define EXPECT_FALSE(actual) EXPECT_EQ_BASE((actual) == 0, "false", "true", "%s")
 
 namespace details {
 void test_number(double expect_number, const char* json) {
@@ -39,6 +42,13 @@ void test_error(Status error, const char* json) {
 	LeptJSON v(json, ValueType::FALSE_TYPE);
 	EXPECT_EQ_INT(error, v.parse());
 	EXPECT_EQ_INT(ValueType::NULL_TYPE, v.get_type());
+}
+
+void test_string(std::string_view expect_string, const char* json) {
+	LeptJSON v(json);
+	EXPECT_EQ_INT(Status::PARSE_OK, v.parse());
+	EXPECT_EQ_INT(ValueType::STRING_TYPE, v.get_type());
+	EXPECT_EQ_STRING(expect_string, v.get_string());
 }
 }
 
@@ -80,6 +90,23 @@ static void test_parse_number() {
 	details::test_number(1.234E+10, "1.234E+10");
 	details::test_number(1.234E-10, "1.234E-10");
 	details::test_number(0.0, "1e-10000"); /* must underflow */
+
+	details::test_number(1.0000000000000002, "1.0000000000000002"); /* the smallest number > 1 */
+	details::test_number(4.9406564584124654e-324, "4.9406564584124654e-324"); /* minimum denormal */
+	details::test_number(-4.9406564584124654e-324, "-4.9406564584124654e-324");
+	details::test_number(2.2250738585072009e-308, "2.2250738585072009e-308");  /* Max subnormal double */
+	details::test_number(-2.2250738585072009e-308, "-2.2250738585072009e-308");
+	details::test_number(2.2250738585072014e-308, "2.2250738585072014e-308");  /* Min normal positive double */
+	details::test_number(-2.2250738585072014e-308, "-2.2250738585072014e-308");
+	details::test_number(1.7976931348623157e+308, "1.7976931348623157e+308");  /* Max double */
+	details::test_number(-1.7976931348623157e+308, "-1.7976931348623157e+308");
+}
+
+void test_parse_string() {
+	details::test_string("", "\"\"");
+	details::test_string("Hello", "\"Hello\"");
+	details::test_string("Hello\nWorld", "\"Hello\\nWorld\"");
+	details::test_string("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
 }
 
 static void test_parse_expect_value() {
@@ -116,18 +143,78 @@ static void test_parse_number_too_big() {
 	details::test_error(Status::PARSE_NUMBER_TOO_BIG, "-1e309");
 }
 
+static void test_parse_missing_quotation_mark() {
+	details::test_error(Status::PARSE_MISS_QUOTATION_MARK, "\"");
+	details::test_error(Status::PARSE_MISS_QUOTATION_MARK, "\"abc");
+}
+
+static void test_parse_invalid_string_escape() {
+	details::test_error(Status::PARSE_INVALID_STRING_ESCAPE, "\"\\v\"");
+	details::test_error(Status::PARSE_INVALID_STRING_ESCAPE, "\"\\'\"");
+	details::test_error(Status::PARSE_INVALID_STRING_ESCAPE, "\"\\0\"");
+	details::test_error(Status::PARSE_INVALID_STRING_ESCAPE, "\"\\x12\"");
+}
+
+static void test_parse_invalid_string_char() {
+	details::test_error(Status::PARSE_INVALID_STRING_CHAR, "\"\x01\"");
+	details::test_error(Status::PARSE_INVALID_STRING_CHAR, "\"\x1F\"");
+}
+
+static void test_access_null() {
+	LeptJSON v;
+	v.set_string("a");
+	v.set_nullptr();
+	EXPECT_EQ_INT(ValueType::NULL_TYPE, v.get_type());
+}
+
+static void test_access_boolean() {
+	LeptJSON v;
+	v.set_string("a");
+	v.set_boolean(true);
+	EXPECT_TRUE(v.get_boolean());
+	v.set_boolean(false);
+	EXPECT_FALSE(v.get_boolean());
+}
+
+static void test_access_number() {
+	LeptJSON v;
+	v.set_string("a");
+	v.set_number(1234.5);
+	EXPECT_EQ_DOUBLE(1234.5, v.get_number());
+}
+
+static void test_access_string() {
+	LeptJSON v;
+	v.set_string("");
+	EXPECT_EQ_STRING("", v.get_string());
+	v.set_string("Hello");
+	EXPECT_EQ_STRING("Hello", v.get_string());
+}
+
 static void test_parse() {
 	test_parse_null();
 	test_parse_true();
 	test_parse_false();
 	test_parse_number();
+	test_parse_string();
 	test_parse_expect_value();
 	test_parse_invalid_value();
 	test_parse_root_not_singular();
 	test_parse_number_too_big();
+	test_parse_missing_quotation_mark();
+	test_parse_invalid_string_escape();
+	test_parse_invalid_string_char();
+
+	test_access_null();
+	test_access_boolean();
+	test_access_number();
+	test_access_string();
 }
 
 int main() {
+#ifdef _WINDOWS
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 	test_parse();
 	printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
 	return main_ret;
